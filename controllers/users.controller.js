ddb5245858c73f89d50a17816a6e31c9d93eb96a
Digvijay Singh <dig.malik1911@gmail.com>
2024-10-01 03:33:41 +0530

    its on steroid

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,12 +1,12 @@
 const users = require('../models/users.model.js');
 const jwt = require('jsonwebtoken');
 
-const tokenSecretKey = "49194f270d1bb055f9437bf79029bfef6afa4b798bb9a63964a9ce0654e76bebbd9492d7135fbfd48f2532eb31e026f5c11a5b7911e5fdad05d5290f48d9b7eef3a68aa1dd2f5f2537ec57e83ecca48f3a6a2075c1ae59b5bf941f072dfad81951efe0cdd90f90e43b7c6746a08936184122296b9fccee22ba34c20853747c7a16b4a36bd45ea75ec32cad2915808e769f9b5f06d1aaa3f55e1b075d6f65d636fa391908f023d1e7b8c2fbb4b566401b5e40b5eb2c7475c6c17c937cdfa1aae77d971e370f620922ded4c3a68c8920c1943f969de8ca605bf05cc2befc37b7e39d47764669b5828b3f30061847a11e7a9b2b930852a46831abbad9d36d9b3a5d";
+const tokenSecretKey = "dd2fe518-149d-4e1e-823a-673335131ce5";
 
 //Sign Up
 const signUp = async (req, res) => {
   try{
-    const { userName, userOwnerName, userEmail, userPassword, isAdmin } = req.body;
+    const { userName,  userEmail, userPassword } = req.body;
     const user = new users({
       userName,
       userEmail,
