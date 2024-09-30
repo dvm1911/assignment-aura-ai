a03f28adb640270edf48b989dbf21395f34d0b11
Digvijay Singh <dig.malik1911@gmail.com>
2024-10-01 03:36:10 +0530

    its on steroid

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {  await mongoose.connect(
-    "mongodb+srv://coinScoreBE19:pxCS1911DB@coinscoredb.00jflct.mongodb.net/?retryWrites=true&w=majority&appName=coinScoreDB");
+    "mongodb+srv://digmalik1911:dv_m1911@cluster0.rkthh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
   }catch(e){
     console.log("Can not connect to database" + e);
   }
