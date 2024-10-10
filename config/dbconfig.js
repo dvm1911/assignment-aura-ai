44eb9388fe7305c17e3ab9d51c6b195f989f922c
Digvijay Singh <dig.malik1911@gmail.com>
2024-10-10 20:56:18 +0530

    final draft

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {  await mongoose.connect(
-    "mongodb+srv://digmalik1911:dv_m1911@cluster0.rkthh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    "mongodb+srv://docsave1911:dv_m1911@savedoc.lrjm1.mongodb.net/?retryWrites=true&w=majority&appName=savedoc");
   }catch(e){
     console.log("Can not connect to database" + e);
   }
